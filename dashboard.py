bb4c2951507152b8ec44a6dff45ca5a1e8c1eb45
nikolaosvyzas <n.vyzas@tue.nl>
2020-03-02 14:34:17 +0100

    converted date slider to date picker

--- dashboard.py
+++ dashboard.py
@@ -11,137 +11,137 @@ from dash.dependencies import Input, Output
 import plotly.graph_objs as go
 import numpy as np
 import pandas as pd
+from datetime import datetime as dt
 
-df=pd.read_csv('data/data_preprocessed.csv')
-df['date_tech'] = pd.to_datetime(df['date_tech']).dt.date
+# Import data
+df=pd.read_csv('data/threeMonths.csv')
+
+# Preprocessing
+df['datee'] = pd.to_datetime(df['datee']).dt.date
 
 unique_accounts=df['a_key'].unique().tolist()
 unique_accounts.sort()
 
+
 # Launch the application:
 app = dash.Dash(__name__)
 
-"""
+
 # Set the app layout
 app.layout=html.Div([
-    dcc.Dropdown(
-        id='dropdown',
-        options=[{'label': i, 'value': i} for i in unique_accounts],
-        value=unique_accounts[0]
-    ),
-    html.H1(
-        id='output-text',
-    ),
-    dcc.Graph(
-        id='timegraph'
-    ),
-    dcc.Graph(
-        id='freqgraph'
-    )
-    dcc.Graph(
-        id='networkgraph'
-
-    )
- ])
-"""
-######################################################################
-
-app.layout=html.Div([
-            html.Div(
-                className="row",
-                children=[
-                    html.Div(
-                        className="col-4",
-                        children=dcc.Dropdown(
-                             id='dropdown',
-                             options=[{'label': i, 'value': i} for i in unique_accounts],
-                             value=unique_accounts[0]
-                        ),
-                        
-                    ),
-                    html.Div(
-                        className="col-8",
-                        children=dcc.RangeSlider(
-                            id='my-range-slider',
-                            min=1,
-                            max=12,
-                            step=1,
-                            value=[1, 12],
-                            marks={
-                                1: {'label': 'Jan'},
-                                2: {'label': 'Feb'},
-                                3: {'label': 'March'},
-                                4: {'label': 'April'},
-                                5: {'label': 'May'},
-                                6: {'label': 'JUNE'},
-                                7: {'label': 'JULY'},
-                                8: {'label': 'AUGUST'},
-                                9: {'label': 'SEP'},
-                                10: {'label': 'OCT'},
-                                11: {'label': 'NOV'},
-                                12: {'label': 'DEC'}
-                            }
-                        )
-                    )
-                ]
-            ),
-            html.Div(
-                className="row",
-                children=[
-                    html.Div(
-                        className='col-6',
-                        children='Network Graph Here'
-                    ),
-                    html.Div(
-                        className='col-6',
-                        children=[
-                            dcc.Tabs([
-                                dcc.Tab(label='Time', children=[
-                                    dcc.Graph(
-                                        id='timegraph',
-                                        figure={
-                                            'data': [
-                                                {'x': [1, 2, 3], 'y': [4, 1, 2],
-                                                    'type': 'bar', 'name': 'SF'},
-                                                {'x': [1, 2, 3], 'y': [2, 4, 5],
-                                                 'type': 'bar', 'name': u'Montréal'},
-                                            ]
-                                        }
-                                    )
-                                ]),
-                                dcc.Tab(label='Frequency', children=[
-                                    dcc.Graph(
-                                        id='freqgraph',
-                                        figure={
-                                            'data': [
-                                                {'x': [1, 2, 3], 'y': [1, 4, 1],
-                                                    'type': 'bar', 'name': 'SF'},
-                                                {'x': [1, 2, 3], 'y': [1, 2, 3],
-                                                 'type': 'bar', 'name': u'Montréal'},
-                                            ]
-                                        }
-                                    )
-                                ])
+        html.Div(
+            className="row",
+            children=[
+                html.Div(
+                    className="col-3",
+                    children=[
+                        dcc.Dropdown(
+                         id='dropdown',
+                         options=[{'label': i, 'value': i} for i in unique_accounts],
+                         value=unique_accounts[0]
+                         )
+                    ]                        
+                ),
+                html.Div(
+                    className="col-3",
+                    children=[
+                        dcc.Dropdown(
+                         id='dropdown_2',
+                         options=[{'label': i, 'value': i} for i in unique_accounts],
+                         value=unique_accounts[0]
+                         )
+                    ]                        
+                ),
+                html.Div(
+                    className="col-6",
+                    children=[
+                        dcc.DatePickerRange(
+                            min_date_allowed=df['datee'].min(),
+                            max_date_allowed=df['datee'].max(),
+                            start_date=df['datee'].min(),
+                            end_date=df['datee'].max(),
+                            display_format='DD MM YYYY',
+                            start_date_placeholder_text="Start Date",
+                            end_date_placeholder_text="End Date",
+                            calendar_orientation='vertical',
+                        )  
+                    ]
+                )
+            ]
+        ),
+        html.Div(
+            className="row",
+            children=[
+                html.Div(
+                    className='col-6',
+                    children='Network Graph Here'
+                ),
+                html.Div(
+                    className='col-6',
+                    children=[
+                        dcc.Tabs([
+                            dcc.Tab(label='Time', children=[
+                                dcc.Graph(
+                                    id='timegraph'
+                                ),
+                                dcc.Graph(
+                                    id='timegraph_2'
+                                ),
+                                dcc.Graph(
+                                    id='timegraph_3',
+                                    figure={
+                                        'data': [
+                                            {'x': [1, 2, 3], 'y': [4, 1, 2],
+                                                'type': 'bar', 'name': 'SF'},
+                                            {'x': [1, 2, 3], 'y': [2, 4, 5],
+                                             'type': 'bar', 'name': u'Montréal'},
+                                        ]
+                                    }
+                                )
+                            ]),
+                            dcc.Tab(label='Frequency', children=[
+                                dcc.Graph(
+                                    id='freqgraph',
+                                    figure={
+                                        'data': [
+                                            {'x': [1, 2, 3], 'y': [1, 4, 1],
+                                                'type': 'bar', 'name': 'SF'},
+                                            {'x': [1, 2, 3], 'y': [1, 2, 3],
+                                             'type': 'bar', 'name': u'Montréal'},
+                                        ]
+                                    }
+                                )
                             ])
-                        ]
-                    )
-                ]
-            )
+                        ])
+                    ]
+                )
+            ]
+        )
 ])
 
-######################################################################
+
 # Define callbacks
+@app.callback(
+    Output('dropdown_2','options'),
+    [Input('dropdown','value')])
+def update_dropdown(key):
+    df_key=df[(df['a_key']==key) | (df['b_key']==key)].sort_values('datee')
+    accounts_key=df_key['a_key'].append(df_key['b_key'],ignore_index=True)
+    options=[{'label': i, 'value': i} for i in accounts_key.unique()]
+    return options
+
+
 
 @app.callback(
     Output('timegraph','figure'),
     [Input('dropdown','value')])
 def update_timegraph(key):
-    df_key=df[(df['a_key']==key) | (df['b_key']==key)].sort_values('date_tech')
+    df_key=df[(df['a_key']==key) | (df['b_key']==key)].sort_values('datee')
     df_key['cum_amount']=df_key['boeking_eur'].cumsum()
     
     data=[
         go.Scatter(
-            x=df_key['date_tech'],
+            x=df_key['datee'],
             y=df_key['boeking_eur'],
             mode='markers',
             marker={
@@ -152,7 +152,86 @@ def update_timegraph(key):
             name='Transactions'
         ),
         go.Scatter(
-            x = df_key['date_tech'],
+            x = df_key['datee'],
+            y = df_key['cum_amount'],
+            mode = 'markers+lines',
+            marker={
+            # 'size': 2.5,
+            # 'opacity': 1,
+            # 'line': {'width': 0.5, 'color': 'white'}
+            },
+            name = 'Cumulative amount'
+        )
+    ]
+    
+    layout= go.Layout(
+        xaxis={'title': 'Date'},
+        yaxis={'title': 'Amount'},
+        margin={'l': 40, 'b': 40, 't': 10, 'r': 0},
+        hovermode='closest'
+    )
+
+    return {'data':data,'layout':layout}
+
+@app.callback(
+    Output('timegraph_2','figure'),
+    [Input('dropdown_2','value')])
+def update_timegraph_2(key):
+    df_key=df[(df['a_key']==key) | (df['b_key']==key)].sort_values('datee')
+    df_key['cum_amount']=df_key['boeking_eur'].cumsum()
+    
+    data=[
+        go.Scatter(
+            x=df_key['datee'],
+            y=df_key['boeking_eur'],
+            mode='markers',
+            marker={
+                'size': 10,
+                'opacity': 0.3,
+                'line': {'width': 0.5, 'color': 'white'}
+            },
+            name='Transactions'
+        ),
+        go.Scatter(
+            x = df_key['datee'],
+            y = df_key['cum_amount'],
+            mode = 'markers+lines',
+            marker={
+            # 'size': 2.5,
+            # 'opacity': 1,
+            # 'line': {'width': 0.5, 'color': 'white'}
+            },
+            name = 'Cumulative amount'
+        )
+    ]
+    
+    layout= go.Layout(
+        xaxis={'title': 'Date'},
+        yaxis={'title': 'Amount'},
+        margin={'l': 40, 'b': 40, 't': 10, 'r': 0},
+        hovermode='closest'
+    )
+
+    return {'data':data,'layout':layout}
+
+def update_timegraph_3(key):
+    df_key=df[(df['a_key']==key) | (df['b_key']==key)].sort_values('datee')
+    df_key['cum_amount']=df_key['boeking_eur'].cumsum()
+    
+    data=[
+        go.Scatter(
+            x=df_key['datee'],
+            y=df_key['boeking_eur'],
+            mode='markers',
+            marker={
+                'size': 10,
+                'opacity': 0.3,
+                'line': {'width': 0.5, 'color': 'white'}
+            },
+            name='Transactions'
+        ),
+        go.Scatter(
+            x = df_key['datee'],
             y = df_key['cum_amount'],
             mode = 'markers+lines',
             marker={
@@ -177,8 +256,8 @@ def update_timegraph(key):
     Output('freqgraph','figure'),
     [Input('dropdown','value')])
 def update_freqgraph(key):
-    df_key=df[(df['a_key']==key) | (df['b_key']==key)].sort_values('date_tech')
-    intervals=df_key['date_tech']-df_key['date_tech'].shift(1)
+    df_key=df[(df['a_key']==key) | (df['b_key']==key)].sort_values('datee')
+    intervals=df_key['datee']-df_key['datee'].shift(1)
     
     data=[
         go.Histogram(
